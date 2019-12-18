578575b7327b2aeca818e586145e26bdebcd8d48
hrsh_panwar <hwardhan337@gmail.com>
2019-12-18 23:40:38 +0530

    uname added

--- uname.sh
+++ uname.sh
@@ -1,6 +1,19 @@
-#!/bin/bash
-# Username
-
+#!/bin/bash set-e
+# Username 
+IFS=':'
 if [ $# -eq  2 ]
-    
+then
+    input=$1
+    Done=false
+    until $Done
+    do
+        read line || Done=true;
+        readarray -d : -t strarr <<< "$line"
+        if [ ${strarr[0]} = $2 ]
+        then 
+            echo ${strarr[4]}
+        fi    
+    done < $input
+else
+    exit -1
 fi
